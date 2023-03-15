e7a8b64bce4bccb117c88ff26391ba8ea75d7272
alizzaafra <100418478+alizzaafra@users.noreply.github.com>
2023-03-15 11:00:46 +0700

    Create Poligon.java

---
+++ Praktikum 3/org/poligon/Poligon.java
@@ -0,0 +1,16 @@
+/*
+	  Poligon.java 15/03/2023
+	  Nama      : Alizza Afra Afifah
+	  NIM       : 24060121140135
+	  Deskripsi : Representasi dasar dari objek poligon (segi banyak)
+*/
+  
+package org.poligon;
+
+public class Poligon{
+	protected int jumlahSisi;
+	
+	public int getJumlahSisi(){
+		return this.jumlahSisi;
+	}
+}
