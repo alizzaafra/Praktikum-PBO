c9e24b6109ba388e54ff5a248ec6c5e49f832e6d
alizzaafra <100418478+alizzaafra@users.noreply.github.com>
2023-03-15 10:54:08 +0700

    Create PersegiPanjang.java

---
+++ Praktikum 3/org/bangundatar/PersegiPanjang.java
@@ -0,0 +1,29 @@
+/*
+	  PersegiPanjang.java 15/03/2023
+	  Nama      : Alizza Afra Afifah
+	  NIM       : 24060121140135
+	  Deskripsi : Representasi  dari objek persegi panjang, turunan kelas poligon
+*/
+  
+package org.bangundatar;
+
+import org.poligon.Poligon;
+
+public class PersegiPanjang extends Poligon{
+	private double panjang,lebar;
+	
+	public PersegiPanjang(double panjang, double lebar, int  jumlahSisi){
+		this.panjang = panjang;
+		this.lebar = lebar;
+		this.jumlahSisi = jumlahSisi;
+	}
+	
+	public double hitungLuas(){
+		return panjang * lebar;
+	}
+	
+	public void printInfo(){
+		System.out.println("Bangun Persegi Panjang bersisi "+this.getJumlahSisi());
+	}
+
+}
