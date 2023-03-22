c01f2dea3eb35b5259798dff5f4c0e02c9da1931
alizzaafra <100418478+alizzaafra@users.noreply.github.com>
2023-03-22 20:40:14 +0700

    Create MKubus.java

---
+++ Praktikum 4/org/main/MKubus.java
@@ -0,0 +1,21 @@
+/*
+	  MKubus.java 22/03/2023
+	  Nama      : Alizza Afra Afifah
+	  NIM       : 24060121140135
+	  Deskripsi : Main dari kubus
+*/
+  
+package org.main;
+
+import org.bangundatar.BujurSangkar;
+import org.bangunruang.Kubus;
+
+public class MKubus {
+    public static void main(String[] args) {
+              BujurSangkar bujurSangkar = new BujurSangkar(4);
+              Kubus kubus = new Kubus (bujurSangkar);
+
+              // TODO
+             // Print luas dan volume kubus
+        }
+}
