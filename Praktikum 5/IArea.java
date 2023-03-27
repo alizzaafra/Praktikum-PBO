a1da6c4c27af79009d72be1f4666d67c2654f7c1
alizzaafra <100418478+alizzaafra@users.noreply.github.com>
2023-03-28 02:01:19 +0700

    Create IArea.java

---
+++ Praktikum 5/IArea.java
@@ -0,0 +1,12 @@
+/*
+	  IArea.java 28/03/2023
+	  Nama      : Alizza Afra Afifah
+	  NIM       : 24060121140135
+    Deskripsi : Interface yang mendefinisikan kontrak bahwa setiap kelas 
+			          yang membuat implementasi dari interface ini harus memiliki cara
+			          untuk menghitung luas
+*/
+
+public interface IArea{
+	public double hitungLuas();
+}
