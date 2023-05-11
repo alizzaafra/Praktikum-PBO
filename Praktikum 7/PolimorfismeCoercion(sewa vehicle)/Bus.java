1ad299b698cd33248be2e0f93b7c1e5a57ac6cc6
alizzaafra <100418478+alizzaafra@users.noreply.github.com>
2023-05-11 21:11:25 +0700

    Create Bus.java

---
+++ Praktikum 7/PolimorfismeCoercion(sewa vehicle)/Bus.java
@@ -0,0 +1,10 @@
+/*
+	  Bus.java 10/05/2023
+	  Nama      : Alizza Afra Afifah
+	  NIM       : 24060121140135
+	  Deskripsi : Kelas yang berisi atribut dan metode dari sub kelas bus
+*/
+
+public class Bus extends Vehicle {
+
+}
