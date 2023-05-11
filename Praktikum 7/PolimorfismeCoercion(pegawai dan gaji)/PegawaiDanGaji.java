58a35e4dcf88617c3092b1688e1a765cc6cb6a32
alizzaafra <100418478+alizzaafra@users.noreply.github.com>
2023-05-11 21:20:03 +0700

    Create PegawaiDanGaji.java

---
+++ Praktikum 7/PolimorfismeCoercion(pegawai dan gaji)/PegawaiDanGaji.java
@@ -0,0 +1,20 @@
+/*
+	  PegawaiDanGaji.java 10/05/2023
+	  Nama      : Alizza Afra Afifah
+	  NIM       : 24060121140135
+	  Deskripsi : Main class untuk menjalankan class Pegawai dan class Manajer
+*/
+
+
+public class PegawaiDanGaji {
+    public static void main(String[] args) {
+        Pegawai pegawai = new Programmer("Mira");
+        Pegawai pegawai2 = new Manajer("Joko");
+        Pegawai pegawai3 = new Manajer("Argo");
+
+        Payroll payroll = new Payroll();
+        payroll.cetakGaji(pegawai);
+        payroll.cetakGaji(pegawai2);
+        payroll.cetakGaji(pegawai3);
+    }
+}
