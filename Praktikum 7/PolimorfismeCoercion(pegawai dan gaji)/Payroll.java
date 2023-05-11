33ece123ce1d2dc008f67ec2c64aa83fb94f8a41
alizzaafra <100418478+alizzaafra@users.noreply.github.com>
2023-05-11 21:22:57 +0700

    Create Payroll.java

---
+++ Praktikum 7/PolimorfismeCoercion(pegawai dan gaji)/Payroll.java
@@ -0,0 +1,12 @@
+/*
+	  Payroll.java 10/05/2023
+	  Nama      : Alizza Afra Afifah
+	  NIM       : 24060121140135
+	  Deskripsi : Kelas yang berisi metode untuk mencetak gaji pegawai
+*/
+
+public class Payroll {
+    public void cetakGaji(Pegawai p) {
+        p.tampilData();
+    }
+}
