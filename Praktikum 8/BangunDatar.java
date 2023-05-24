32d06a5b80b92beeb5506db8106b6cb6282c5230
alizzaafra <100418478+alizzaafra@users.noreply.github.com>
2023-05-24 15:35:57 +0700

    Create BangunDatar.java

---
+++ Praktikum 8/BangunDatar.java
@@ -0,0 +1,9 @@
+/**
+ * File : BangunDatar.java
+ * Penulis : Alizza Afra Afifah / 24060121140135
+ * Deskripsi : kelas abstrak untuk bangun datar
+ */
+
+public abstract class BangunDatar{
+    public abstract double hitungKeliling();
+}
